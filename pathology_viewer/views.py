6ae7637a73de4874c8034d5a4fa6d944363fbccb
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2021-05-17 11:55:59 +0530

    added files

--- pathology_viewer/views.py
+++ pathology_viewer/views.py
@@ -27,8 +27,8 @@ def wsi_upload(request):
         wsi_location = media_url+filename 
         abs_path = os.path.abspath("../")+r"\pathology_project\media"
         wsi_path = abs_path+'\\'+filename
-        image = pyvips.Image.new_from_file(wsi_path,access='sequential')
-        image.dzsave(wsi_path,tile_height=256,tile_width=256,overlap=0)
+        #image = pyvips.Image.new_from_file(wsi_path,access='sequential')
+        #image.dzsave(wsi_path,tile_height=256,tile_width=256,overlap=0)
         path = settings.MEDIA_ROOT
         #img_list = os.listdir(abs_path)
         context = {'uploaded_file_url': uploaded_file_url}
